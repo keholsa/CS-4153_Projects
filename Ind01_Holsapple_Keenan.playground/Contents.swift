86a23d8d4e67d933a64c1c29f8d587cfb481da60
keholsa <99051518+keholsa@users.noreply.github.com>
2023-02-03 19:07:47 -0600

    Update Contents.swift

--- Ind01_Holsapple_Keenan.playground/Contents.swift
+++ Ind01_Holsapple_Keenan.playground/Contents.swift
@@ -1,12 +1,13 @@
-
-
+// Ind01_Holsapple_Keenan.playground
+// Indivdiual 01 Assignment: Practicing Object Oriented Programming in Playground model of Swift
+ 
 class Cash{
    
     // initializing array for fixed cash values
     let comparisonArrMoneyOptions:[Double] = [50.0, 20.0, 10.0, 5.0, 1.0, 0.25, 0.1, 0.05, 0.01]
     
     //property to be taken in from functions at bottom
-    var moneyAmount: Double
+    var moneyAmount: Double 
     
     // declaration of getter
     func getBillCountArr() -> [Int]{
@@ -15,7 +16,7 @@ class Cash{
         
         
         var i = 0
-        var billCountArr = [0,0,0,0,0,0,0,0,0]
+        var billCountArr = [Int](repeating: 0, count: 9)
         let emptyIntArr = [Int]()
         
 
@@ -45,53 +46,111 @@ class Cash{
         return billCountArr
     }
 
-        // set(cashCountArr){
-        //     var i = 0
+    // Setter function that reads in array value and returns moneyAmount as object of class with
+    // new computed value
+    func setMoneyAmount(cashCountArr: [Int]){
+        
+        // increment for while loop declaration
+        var i = 0
+        
+        // if statement that determines if array size inputed from line is less than 9
+        if cashCountArr.count < 9{
+            moneyAmount = -1
+        }
+        // performs normal execution of function with valid array size
+        else{
             
-        //     while i < 8{
-        //         i += 1
+            // loop increments at size of array (predefined)
+            while i < 9{
+            
+                // algorithm that adds on previous moneyAmount by the multiple of how many dollar bills exist
+                moneyAmount = moneyAmount + (Double(cashCountArr[i]) * comparisonArrMoneyOptions[i])
+            
+                // increment of loop
+                i += 1
                 
-        //     }
-                
-        // }
+            }
+        }
+            
+    }
     
         
-        //constructor for moneyAmount property
-        init(moneyAmount: Double){
-            self.moneyAmount = moneyAmount
-        }
+    // //constructor for moneyAmount property
+    init(moneyAmount: Double){
+        self.moneyAmount = moneyAmount
+    }
 }
 
 // section for testing values of getter in class
 var cash1 = Cash(moneyAmount: -1)
 var cash1Getter = cash1.getBillCountArr()
-print(cash1Getter)
 
 var cash2 = Cash(moneyAmount: 0)
 var cash2Getter = cash2.getBillCountArr()
-print(cash2Getter)
 
 var cash3 = Cash(moneyAmount: 84.26)
 var cash3Getter = cash3.getBillCountArr()
-print(cash3Getter)
 
 var cash4 = Cash(moneyAmount: 32.59)
 var cash4Getter = cash4.getBillCountArr()
-print(cash4Getter)
 
 var cash5 = Cash(moneyAmount: 69.89)
 var cash5Getter = cash5.getBillCountArr()
-print(cash5Getter)
 
 var cash6 = Cash(moneyAmount: 95.12)
 var cash6Getter = cash6.getBillCountArr()
+
+// print statements for the getter tests
+print("*************************************")
+print("Printed Values for testing getters:")
+print("*************************************")
+print(cash1Getter)
+print(cash2Getter)
+print(cash3Getter)
+print(cash4Getter)
+print(cash5Getter)
 print(cash6Getter)
 
-// section for testing setter quantities of class
-//TOOD: need to add printouts for get assignments (probably needs more code on setter part)
-//let testSetterArr1 = [1, 0, 3, 2, 5, 8]
-//let testSetterArr2 = [1, 5, 2, 3, 1, 4, 6, 3, 0]
-//let testSetterArr3 = [3, 4, 2, 5, 9, 1, 5, 4, 3]
-//let testSetterArr4 = [4, 2, 5, 3, 2, 7, 5, 8, 2]
-//let testSetterArr5 = [3, 5, 6, 2, 4, 7, 2, 8, 6]
-//let testSetterArr6 = [6, 3, 2, 7, 8, 3, 2, 6, 7]
+// defining arrays that will be used in setter tests
+let testSetterArr1 = [0, 6, 4, 2, 8, 4]
+let testSetterArr2 = [1, 5, 2, 3, 1, 4, 6, 3, 0]
+let testSetterArr3 = [3, 4, 2, 5, 9, 1, 5, 4, 3]
+let testSetterArr4 = [4, 2, 5, 3, 2, 7, 5, 8, 2]
+let testSetterArr5 = [3, 5, 6, 2, 4, 7, 2, 8, 6]
+let testSetterArr6 = [6, 3, 2, 7, 8, 3, 2, 6, 7]
+
+print("*************************************")
+print("Printed Values for testing setters:")
+print("*************************************")
+
+// setter testing assignments
+let cash1Setter: () = cash1.setMoneyAmount(cashCountArr: testSetterArr1)
+print("Cash amount after first array setter: ", cash1.moneyAmount)
+// performs getter with new money amount on object variable
+cash1Getter = cash1.getBillCountArr()
+print(cash1Getter)
+
+let cash2Setter: () = cash2.setMoneyAmount(cashCountArr: testSetterArr2)
+print("Cash amount after second array setter: ", cash2.moneyAmount)
+cash2Getter = cash2.getBillCountArr()
+print(cash2Getter)
+
+let cash3Setter: () = cash3.setMoneyAmount(cashCountArr: testSetterArr3)
+print("Cash amount after third array setter: ", cash3.moneyAmount)
+cash3Getter = cash3.getBillCountArr()
+print(cash3Getter)
+
+let cash4Setter: () = cash4.setMoneyAmount(cashCountArr: testSetterArr4)
+print("Cash amount after fourth array setter: ", cash4.moneyAmount)
+cash4Getter = cash4.getBillCountArr()
+print(cash4Getter)
+
+let cash5Setter: () = cash5.setMoneyAmount(cashCountArr: testSetterArr5)
+print("Cash amount after fifth array setter: ", cash5.moneyAmount)
+cash5Getter = cash5.getBillCountArr()
+print(cash5Getter)
+
+let cash6Setter: () = cash6.setMoneyAmount(cashCountArr: testSetterArr6)
+print("Cash amount after sixth array setter: ", cash6.moneyAmount)
+cash6Getter = cash6.getBillCountArr()
+print(cash6Getter)
